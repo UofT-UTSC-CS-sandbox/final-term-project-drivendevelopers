2990c5050da111e526e95948950a72b6b875e6b0
Harish <hthevakumaran@gmail.com>
2024-08-07 08:13:18 -0400

    Login Test

--- edu-prod-REAL/src/App.test.js
+++ edu-prod-REAL/src/App.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders login page', () => {
-  const { getByText } = render(
+test('renders login button', () => {
+  render(
     <MemoryRouter initialEntries={['/']}>
       <App />
     </MemoryRouter>
   );
-  const loginElement = getByText(/login/i);
-  expect(loginElement).toBeInTheDocument();
+  const loginButton = screen.getByRole('button', { name: /login/i });
+  expect(loginButton).toBeInTheDocument();
 });
