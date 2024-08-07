2f167d4f51a66dfc346e71dfeb6611bb6f6ffd8a
Harish <hthevakumaran@gmail.com>
2024-08-07 00:08:12 -0400

    Remove jsx and js from extentionstotreatasesm array

--- edu-prod-REAL/jest.config.cjs
+++ edu-prod-REAL/jest.config.cjs
@@ -1,20 +1,19 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    transform: {
-      "^.+\\.[tj]sx?$": "babel-jest",
+  testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(axios|preact)/)"
+  ],
+  moduleNameMapper: {
+    "^axios$": require.resolve('axios'),
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  globals: {
+    'ts-jest': {
+      useESM: true,
     },
-    transformIgnorePatterns: [
-      "/node_modules/(?!(axios|preact)/)"
-    ],
-    moduleNameMapper: {
-      "^axios$": require.resolve('axios'),
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-    },
-    extensionsToTreatAsEsm: [".ts", ".tsx", ".js", ".jsx"],
-    globals: {
-      'ts-jest': {
-        useESM: true,
-      },
-    },
-  };
-  
+  },
+};
