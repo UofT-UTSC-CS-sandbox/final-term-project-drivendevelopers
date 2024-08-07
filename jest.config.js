fbe93c26db7fbfd11ef02ba261800e3d58640169
Harish <hthevakumaran@gmail.com>
2024-08-06 23:38:39 -0400

    Docker update

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,14 @@
-// jest.config.js
 module.exports = {
     testEnvironment: 'jsdom',
     transform: {
       "^.+\\.[tj]sx?$": "babel-jest",
     },
     transformIgnorePatterns: [
-      "/node_modules/(?!(axios)/)",
+      "/node_modules/(?!(axios|preact)/)"
     ],
     moduleNameMapper: {
-      '^axios$': require.resolve('axios'),
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+      "^axios$": require.resolve('axios'),
+      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
     },
     extensionsToTreatAsEsm: [".ts", ".tsx", ".js", ".jsx"],
     globals: {
