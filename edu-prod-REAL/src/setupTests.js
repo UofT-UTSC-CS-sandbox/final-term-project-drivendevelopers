ca10e05db75fb2c4826d864866b1d7d235bc4ff4
Harish <hthevakumaran@gmail.com>
2024-08-07 02:02:38 -0400

    NPM the jest-dom

--- edu-prod-REAL/src/setupTests.js
+++ edu-prod-REAL/src/setupTests.js
@@ -1,5 +1,3 @@
-// src/setupTests.js
 import '@testing-library/jest-dom/extend-expect';
 const { TextEncoder, TextDecoder } = require('util');
-
 Object.assign(global, { TextDecoder, TextEncoder });
