382552425afd8a8b84f231a20c222fa40c541904
Harish <hthevakumaran@gmail.com>
2024-08-07 00:39:30 -0400

    jest config change

--- edu-prod-REAL/jest.config.cjs
+++ edu-prod-REAL/jest.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.[tj]sx?$": "babel-jest",
+    "^.+\\.[tj]sx?$": "babel-jest"
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!(axios|preact|@fullcalendar)/)"
+    "/node_modules/(?!axios|preact|@fullcalendar)"
   ],
   moduleNameMapper: {
     "^axios$": require.resolve('axios'),
@@ -13,7 +13,7 @@ module.exports = {
   extensionsToTreatAsEsm: [".ts", ".tsx"],
   globals: {
     'ts-jest': {
-      useESM: true,
-    },
-  },
+      useESM: true
+    }
+  }
 };
