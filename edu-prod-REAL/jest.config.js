b5fa3cac148fe540d09b18b2f450820f3006ce2a
Harish <hthevakumaran@gmail.com>
2024-08-07 07:27:17 -0400

    Revert back to working ES Module

--- edu-prod-REAL/jest.config.js
+++ edu-prod-REAL/jest.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  testEnvironment: 'jsdom',
   transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-      '^.+\\.jsx?$': 'babel-jest',
-      '^.+\\.mjs$': 'babel-jest'
+    "^.+\\.[tj]sx?$": "babel-jest",
+    "^.+\\.mjs$": "babel-jest", // Add this line to transform .mjs files
   },
   transformIgnorePatterns: [
-      '/node_modules/(?!axios|preact|@fullcalendar)'
-  ]
+    "/node_modules/(?!axios|preact|@fullcalendar)"
+  ],
+  moduleNameMapper: {
+    "^axios$": require.resolve('axios'),
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^preact$": require.resolve('preact'),
+    "^preact-render-to-string$": require.resolve('preact-render-to-string')
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx", ".mjs"], // Add .mjs here
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"]
 };
