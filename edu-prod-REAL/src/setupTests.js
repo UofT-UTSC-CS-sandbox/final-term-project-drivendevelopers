3244f787f8fa1dd1ad6985e902044497103c42e1
Harish <hthevakumaran@gmail.com>
2024-08-07 02:23:02 -0400

    Make expect imported globally to tests

--- edu-prod-REAL/src/setupTests.js
+++ edu-prod-REAL/src/setupTests.js
@@ -2,3 +2,4 @@
 import '@testing-library/jest-dom';
 const { TextEncoder, TextDecoder } = require('util');
 Object.assign(global, { TextDecoder, TextEncoder });
+import { expect } from '@jest/globals';
